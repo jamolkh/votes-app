e9bd526455a1b4c42302c36c84dd8f83b841085b
Jamol <jamolkh@gmail.com>
2021-04-16 10:33:39 +0000

    5th update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -95,7 +95,7 @@
                     </button>
                 </div>
 
-            </div>
+                </div>
            <div class="flex px-2 py-6">
                <a href="" class="flex-none">
                    <img src="https://source.unsplash.com/200x200/?face&crop=face&s=1" alt="avatar" class="w-14 h-14 rounded-xl">
